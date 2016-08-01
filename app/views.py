ab0440cdbe0475bce05e5d2e62940ecb2ad08fef
Gui Talarico <gtalarico@gmail.com>
2016-08-01 01:59:39 -0400

    Added site map generator

--- app/views.py
+++ app/views.py
@@ -2,7 +2,7 @@ import os
 import sys
 
 from app import app
-from flask import render_template, flash, redirect, url_for, session, request
+from flask import render_template, flash, redirect, url_for, session, request, make_response
 from flask import Flask, abort
 
 
@@ -20,7 +20,7 @@ def index():
     return render_template('index.html', title=title)
 
 
-@app.route('/<int:year>/')
+@app.route('/<int:year>/', methods=["GET"])
 def api(year):
     title = 'Revit API Docs: ' + str(year)
     namespace_year = 'ns_{year}.html'.format(year=year)
@@ -30,14 +30,14 @@ def api(year):
                            namespace_year=namespace_year,
                            content=content)
 
-@app.route('/<int:year>/new')
+@app.route('/<int:year>/new', methods=["GET"])
 def new(year):
     return redirect(url_for('api', year=year))
 
 
 
 # API Pages: /2015/123sda-asds-asd.htmll
-@app.route('/<int:year>/<path:path>')
+@app.route('/<int:year>/<path:path>', methods=["GET"])
 def api_2015(year, path):
     title = 'Revit API ' + str(year)
     html_path = '{year}/{path}'.format(year=year, path=path)
@@ -52,7 +52,7 @@ def api_2015(year, path):
 
 # Need to search and replace to it's served by guinicorn
 # Some icons not being served.
-@app.route('/<path:folder>/<path:path>')
+@app.route('/<path:folder>/<path:path>', methods=["GET"])
 def static_proxy(folder, path):
     try:
         return app.send_static_file(os.path.join(folder, path))
@@ -72,9 +72,29 @@ def server_error(e):
     return render_template('error.html', errormsg=errormsg), 500
 
 
-@app.route('/robots.txt')
-@app.route('/sitemap.xml')
+# @app.route('/sitemap.xml')
+@app.route('/robots.txt', methods=["GET"])
 def static_from_root():
     path = os.path.join('static', request.path[1:])
     return redirect(path, code=302)
     # return app.send_static_file(os.path.join('static', request.path[1:]))
+
+@app.route("/sitemap.xml", methods=["GET"])
+def sitemap():
+    """Generate sitemap.xml """
+    pages = ['http://www.revitapidocs.com/',
+             'http://www.revitapidocs.com/2015/',
+             'http://www.revitapidocs.com/2016/',
+             'http://www.revitapidocs.com/2017']
+    templates = os.path.join('app','templates')
+    for root, subfolder, files in os.walk(templates):
+        for f in files:
+            url = 'http://www.revitapidocs.com/2015/{}'.format(f)
+            if url.endswith('.htm'):
+                pages.append(url)
+                
+    sitemap_xml = render_template('sitemap_template.xml', pages=pages)
+    response = make_response(sitemap_xml)
+    response.headers["Content-Type"] = "application/xml"
+
+    return response
