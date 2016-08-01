7681c90a898855c66b323001844599855d7bb337
Gui Talarico <gtalarico@gmail.com>
2016-08-01 02:10:03 -0400

    individual sitemaps

--- app/views.py
+++ app/views.py
@@ -79,6 +79,25 @@ def static_from_root():
     return redirect(path, code=302)
     # return app.send_static_file(os.path.join('static', request.path[1:]))
 
+@app.route("/<int:year>/sitemap.xml", methods=["GET"])
+def year_sitemap(year):
+    """Generate sitemap.xml """
+    pages = ['http://www.revitapidocs.com/',
+             'http://www.revitapidocs.com/2015/',
+             'http://www.revitapidocs.com/2016/',
+             'http://www.revitapidocs.com/2017']
+    templates = os.path.join('app','templates',str(year))
+    for filename in os.listdir(templates):
+        url = 'http://www.revitapidocs.com/2015/{}'.format(filename)
+        print(filename)
+        pages.append(url)
+
+    sitemap_xml = render_template('sitemap_template.xml', pages=pages)
+    response = make_response(sitemap_xml)
+    response.headers["Content-Type"] = "application/xml"
+
+    return response
+
 @app.route("/sitemap.xml", methods=["GET"])
 def sitemap():
     """Generate sitemap.xml """
@@ -86,13 +105,7 @@ def sitemap():
              'http://www.revitapidocs.com/2015/',
              'http://www.revitapidocs.com/2016/',
              'http://www.revitapidocs.com/2017']
-    templates = os.path.join('app','templates')
-    for root, subfolder, files in os.walk(templates):
-        for f in files:
-            url = 'http://www.revitapidocs.com/2015/{}'.format(f)
-            if url.endswith('.htm'):
-                pages.append(url)
-                
+
     sitemap_xml = render_template('sitemap_template.xml', pages=pages)
     response = make_response(sitemap_xml)
     response.headers["Content-Type"] = "application/xml"
