368dadfd3f01379ab52000936ca5e55f2046e033
Gui Talarico <gtalarico@gmail.com>
2016-08-02 00:29:45 -0400

    Flask Assets and Compress Added

--- app/__init__.py
+++ app/__init__.py
@@ -2,8 +2,17 @@
 import os
 from flask import Flask
 from flask_compress import Compress
+from flask_assets import Bundle, Environment
+
+from app.assets import css_assets, js_assets
+
 app = Flask(__name__)
 Compress(app)
+
+assets = Environment(app)
+assets.debug = True
+assets.register('css_assets', css_assets)
+assets.register('js_assets', js_assets)
 
 from app import views, seo_response, errors
 from app.config import config
---
+++ app/assets.py
@@ -0,0 +1,25 @@
+from flask_assets import Bundle
+
+css_assets = Bundle('css/bootstrap.css',
+                    'css/main.css',
+                    'css/overrides.css',
+                    filters='cssmin',
+                    output='packed/packed.css')
+
+js_assets = Bundle('js/bootstrap.js', 'js/main.js',
+                   filters='rjsmin',
+                   output='packed/packed.js')
+
+
+css_chm = Bundle('styles/presentation.css',
+                 output='packed_chm/packed.css')
+
+js_chm = Bundle('scripts/EventUtilities.js',
+                'scripts/SplitScreen.js',
+                'scripts/Dropdown.js',
+                'scripts/script_manifold.js',
+                'scripts/script_feedBack.js',
+                'scripts/CheckboxMenu.js',
+                'scripts/CommonUtilities.js',
+                filters='rjsmin',
+                output='packed_chm/packed.js')
