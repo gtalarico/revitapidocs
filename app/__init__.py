ef6e813bdd9c6052ca11c65386a80e859e0e501d
Gui Talarico <gtalarico@gmail.com>
2016-08-02 00:14:21 -0400

    Flask Compress

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,10 @@
 '''import main Flask. This starts when run.py calls from app import app'''
 import os
 from flask import Flask
-
+from flask_compress import Compress
 app = Flask(__name__)
+Compress(app)
 
 from app import views, seo_response, errors
 from app.config import config
-
 app.config.from_object(config)
